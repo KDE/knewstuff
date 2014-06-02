5f11abb807702cdf990722d7e14fe802ce6831b8
Marco Martin <notmart@gmail.com>
2014-06-02 15:22:55 +0200

    simplify useless coversion path->url->path

--- src/core/installation.cpp
+++ src/core/installation.cpp
@@ -519,7 +519,7 @@ QStringList Installation::installDownloadedFileAndUncompress(const KNS3::EntryIn
                 success = QFile::remove(installpath);
             }
             if (success) {
-                success = file.rename(QUrl::fromLocalFile(installpath).toLocalFile());
+                success = file.rename(installpath);
                 // qDebug() << "move: " << file.fileName() << " to " << installpath;
             }
             if (!success) {
