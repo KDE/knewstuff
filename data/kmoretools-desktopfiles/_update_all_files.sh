5613fd66c5f42c8acdcb72586292e5fd9346e7b3
Gastón Haro <harogaston@gmail.com>
2020-04-07 12:27:47 -0600

    Replace Vokoscreen with VokoscreenNG
    Summary:
    The original Vokoscreen project has been abandoned
    and superseeded by VokoscreenNG. This patch updates
    the references.
    
    BUG: 416460
    FIXED-IN: 5.70
    
    Test Plan:
    Spectacle -> Tools -> Record Screen the new
    VokoscreenNG entry should appear and the new
    website referenced. (If other tools are installed
    it will appear under "More")
    
    Reviewers: #knewstuff, ngraham, leinir
    
    Reviewed By: ngraham
    
    Subscribers: apol, pino, IlyaBizyaev, kde-frameworks-devel
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D28397

--- data/kmoretools-desktopfiles/_update_all_files.sh
+++ data/kmoretools-desktopfiles/_update_all_files.sh
@@ -59,8 +59,8 @@ cp $DIR_DFLT/org.kde.spectacle.desktop .
 cp $DIR_DFLT/simplescreenrecorder.desktop .
 cp $DIR_DFLT/shutter.desktop .
     cp /usr/share/icons/hicolor/scalable/apps/shutter.svg .
-cp $DIR_DFLT/vokoscreen.desktop .
-    cp /usr/share/pixmaps/vokoscreen.png .
+cp $DIR_DFLT/vokoscreenNG.desktop .
+    cp /usr/share/pixmaps/vokoscreenNG.png .
 cp $DIR_DFLT/xfce4-taskmanager.desktop .
 cp $DIR_DFLT/com.obsproject.Studio.desktop .
 cp $DIR_DFLT/com.obsproject.Studio.png .
