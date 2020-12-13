0fca344c24eab47acbbc73863fd01ba7b56cb11c
Alexander Lohnau <alexander.lohnau@gmx.de>
2020-12-13 21:16:06 +0100

    autotests: Add more tests for the engine

---
+++ autotests/core/knewstuffenginetest.cpp
@@ -0,0 +1,69 @@
+/*
+    This file is part of KNewStuff2.
+    SPDX-FileCopyrightText: 2008 Jeremy Whiting <jpwhiting@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
+
+#include <QTest>
+#include <QtGlobal>
+#include <QSignalSpy>
+
+#include "core/engine.h"
+#include "core/entryinternal.h"
+
+using namespace KNSCore;
+
+class EngineTest: public QObject
+{
+Q_OBJECT
+public:
+    Engine *engine = nullptr;
+    const QString dataDir = QStringLiteral(DATA_DIR);
+
+private Q_SLOTS:
+    void initTestCase();
+    void testPropertiesReading();
+    void testProviderFileLoading();
+};
+
+void EngineTest::initTestCase()
+{
+    engine = new Engine(this);
+    QVERIFY(engine->init(dataDir + "installcommand.knsrc"));
+    QCOMPARE(engine->busyState(), Engine::BusyOperation::LoadingData);
+    QSignalSpy providersLoaded(engine, &Engine::signalProvidersLoaded);
+    QVERIFY(providersLoaded.wait());
+    QCOMPARE(engine->busyState(), Engine::BusyState());
+}
+
+void EngineTest::testPropertiesReading()
+{
+    QCOMPARE(engine->name(), QStringLiteral("InstallCommands"));
+    QCOMPARE(engine->categories(), QStringList({"KDE Wallpaper 1920x1200", "KDE Wallpaper 1600x1200"}));
+    QCOMPARE(engine->useLabel(), QStringLiteral("UseLabelTest"));
+    QVERIFY(engine->hasAdoptionCommand());
+}
+
+void EngineTest::testProviderFileLoading()
+{
+    const QString providerId = QUrl::fromLocalFile(dataDir + "entry.xml").toString();
+    QSharedPointer<Provider> provider = engine->provider(providerId);
+    QVERIFY(provider);
+    QCOMPARE(engine->defaultProvider(), provider);
+
+    KNSCore::EntryInternal::List list;
+    connect(engine, &Engine::signalEntriesLoaded, this,
+            [&list](const KNSCore::EntryInternal::List &loaded){ list = loaded; }, Qt::DirectConnection);
+
+    engine->setSearchTerm(QStringLiteral("Entry 4"));
+    QSignalSpy spy(engine, &Engine::signalEntriesLoaded);
+    QVERIFY(spy.wait());
+    QCOMPARE(list.size(), 1);
+    QCOMPARE(list.constFirst().name(), QStringLiteral("Entry 4 (ghns included)"));
+}
+
+QTEST_MAIN(EngineTest)
+
+#include "knewstuffenginetest.moc"
