0d2dc343322248d31b768fdc940dc4d45a1174b6
Nicolas Fella <nicolas.fella@gmx.de>
2022-08-19 15:43:42 +0200

    Fix appstream id for vokoscreenNG
    BUG: 458064

--- src/kmoretools/kmoretoolspresets.cpp
+++ src/kmoretools/kmoretoolspresets.cpp
@@ -83,7 +83,7 @@ KMoreToolsService *KMoreToolsPresets::registerServiceByDesktopEntryName(KMoreToo
     ADD_ENTRY(QStringLiteral("org.kde.spectacle"),                  0, QStringLiteral("https://kde.org/applications/utilities/org.kde.spectacle"), QStringLiteral("org.kde.spectacle.desktop"));
     ADD_ENTRY(QStringLiteral("simplescreenrecorder"),               0, QStringLiteral("https://www.maartenbaert.be/simplescreenrecorder/"), QStringLiteral("simplescreenrecorder.desktop"));
     ADD_ENTRY(QStringLiteral("com.obsproject.Studio"),              0, QStringLiteral("https://obsproject.com/"), QStringLiteral("com.obsproject.Studio"));
-    ADD_ENTRY(QStringLiteral("vokoscreenNG"),                       0, QStringLiteral("https://github.com/vkohaupt/vokoscreenNG"), QStringLiteral("vokoscreenNG.desktop")); // feature-rich screen recorder
+    ADD_ENTRY(QStringLiteral("vokoscreenNG"),                       0, QStringLiteral("https://github.com/vkohaupt/vokoscreenNG"), QStringLiteral("com.github.vkohaupt.vokoscreenNG")); // feature-rich screen recorder
     ADD_ENTRY(QStringLiteral("xfce4-taskmanager"),                  0, QStringLiteral("https://goodies.xfce.org/projects/applications/xfce4-taskmanager"), QStringLiteral("xfce4-taskmanager.desktop"));
     //
     // ...definitions end
