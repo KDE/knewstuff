9a78cd352a3b75d41f23a156fc7be23ea4f9bd95
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-02-09 10:29:26 +0100

    Clean up CMake config
    - Remove QtQuick config, this is just an empty wrapper that is used nowhere
    - Remove KNS3 from header prefix
    - Remove unused dependencies

--- src/core/jobs/httpworker.cpp
+++ src/core/jobs/httpworker.cpp
@@ -6,7 +6,7 @@
 
 #include "httpworker.h"
 
-#include "../../knewstuffcore_version.h"
+#include "knewstuff_version.h"
 #include "knewstuffcore_debug.h"
 
 #include <QCoreApplication>
@@ -93,7 +93,7 @@ void HTTPWorker::setUrl(const QUrl &url)
 
 static void addUserAgent(QNetworkRequest &request)
 {
-    QString agentHeader = QStringLiteral("KNewStuff/%1").arg(QLatin1String(KNEWSTUFFCORE_VERSION_STRING));
+    QString agentHeader = QStringLiteral("KNewStuff/%1").arg(QLatin1String(KNEWSTUFF_VERSION_STRING));
     if (QCoreApplication::instance()) {
         agentHeader += QStringLiteral("-%1/%2").arg(QCoreApplication::instance()->applicationName(), QCoreApplication::instance()->applicationVersion());
     }
